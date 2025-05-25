50980014c1e785f15573e3f74544a945257087b5
Walhaack <w.haack@duocuc.cl>
2025-05-24 23:41:05 -0400

    update

--- src/main/java/cl/maotech/course_service/controller/CourseController.java
+++ src/main/java/cl/maotech/course_service/controller/CourseController.java
@@ -1,5 +1,27 @@
 package cl.maotech.course_service.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.maotech.course_service.service.CourseService;
+import cl.maotech.course_service.model.Course;
+
+@RestController
+@RequestMapping("/cursos")
 public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @GetMapping
+    public ResponseEntity<List<Course>> LitarCourse() {
+        List<Course> course = courseService.getAllCourses();
+        return ResponseEntity.ok(course);
+    }
 
 }
--- src/main/java/cl/maotech/course_service/repository/CourseRepository.java
+++ src/main/java/cl/maotech/course_service/repository/CourseRepository.java
@@ -1,5 +1,20 @@
 package cl.maotech.course_service.repository;
 
-public interface CourseRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import cl.maotech.course_service.model.Course;
+import java.util.List;
+
+@Repository
+
+public interface CourseRepository extends JpaRepository<Course, Long>{
+
+    public Course findByCodigo(String codigo);
+
+    @Query(value = "SELECT * FROM cursos WHERE nombre = :nombre", nativeQuery = true)
+    List<Course> findByNombreNative(String nombre);
+
 
 }
