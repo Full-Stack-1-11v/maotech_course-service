fee0e5075ce61c27dd70bafdec88823ca506c55a
Walhaack <w.haack@duocuc.cl>
2025-05-25 06:15:07 -0400

    update 1.1

--- src/main/java/cl/maotech/course_service/controller/CourseController.java
+++ src/main/java/cl/maotech/course_service/controller/CourseController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +43,23 @@ public class CourseController {
     public ResponseEntity<Course> guardarCurso(@RequestBody Course course) {
         Course nuevoCurso = courseService.saveCourse(course);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevoCurso);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> actualizarCurso(@PathVariable Long id, @RequestBody Course course) {
+        Course cursoActualizado = courseService.updateCourse(id, course);
+        if (cursoActualizado == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(cursoActualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarCurso(@PathVariable Long id) {
+        if (courseService.getCourseById(id) == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        courseService.deleteCourse(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
